da3f4c22b85505195f8feac6e5922febf7abbac2
Rio Mukhtarom <riomukhtarom10@gmail.com>
2020-07-12 11:18:42 +0800

    Chage to covid

--- android/app/src/main/kotlin/com/riomu/covidupdate/MainActivity.kt
+++ android/app/src/main/kotlin/com/riomu/covidupdate/MainActivity.kt
@@ -1,9 +1,6 @@
-package com.example.aplikasi
+package com.riomu.covidupdate
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
 //    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
